4e006d8c4a811bc852a7ec1e210c79a4dea58a3e
Andrew Hughson <andrew@hughson.me>
2018-07-02 19:46:29 +1200

    Add basic filter

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -17,23 +17,50 @@ class Category extends React.Component<
     productStore: ProductStore;
   }
 > {
+  public state = { filter: '' };
+
   public componentDidMount() {
     this.props.productStore.fetchProducts();
+
+    this.props.categoryStore.selectCategory(this.props.match.params.categoryId);
   }
 
   public componentDidUpdate() {
     this.props.categoryStore.selectCategory(this.props.match.params.categoryId);
   }
 
+  public updateFilter = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
   public render() {
     return (
-      <ul>
-        {this.props.productStore.categoryProducts.map(p => (
-          <li key={p.id}>
-            <Product product={p} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <input
+          type="search"
+          value={this.state.filter}
+          onInput={this.updateFilter}
+        />
+
+        <ul>
+          {this.props.productStore.categoryProducts
+            .filter(
+              c =>
+                !this.state.filter ||
+                c.title
+                  .toLowerCase()
+                  .includes(this.state.filter.toLowerCase()) ||
+                c.description
+                  .toLowerCase()
+                  .includes(this.state.filter.toLowerCase())
+            )
+            .map(p => (
+              <li key={p.id}>
+                <Product product={p} />
+              </li>
+            ))}
+        </ul>
+      </div>
     );
   }
 }
